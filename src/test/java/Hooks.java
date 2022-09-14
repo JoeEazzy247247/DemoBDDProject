68a90d1fd858b1e64d5fd2cb6b0c9fb29b82798c
Joseph Ekeleme <joeeazzy247@gmail.com>
2022-09-14 10:48:04 +0100

    Small change

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -2,6 +2,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.IOException;
 
@@ -16,10 +17,12 @@ public class Hooks extends Driverhelper{
     @Before
     public void setup(){
         WebDriverManager.chromedriver().setup();
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--start-maximized", "--incognito", "--headless");
 //       System.setProperty("webdriver.chrome.driver",
 //                    "C:\\Users\\joeea\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        base.driver = new ChromeDriver();
-        base.driver.manage().window().maximize();
+        base.driver = new ChromeDriver(option);
+        //base.driver.manage().window().maximize();
     }
 
 
